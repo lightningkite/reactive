7e4bdb6efbaee5e0a23beaafdc38c6c974e6fccc
iHoonter <huntdawghero@gmail.com>
2025-08-04 14:12:07 -0600

    Draft factory function documentation

--- src/commonMain/kotlin/com/lightningkite/reactive/core/Draft.kt
+++ src/commonMain/kotlin/com/lightningkite/reactive/core/Draft.kt
@@ -5,7 +5,7 @@ import com.lightningkite.reactive.context.awaitOnce
 
 /**
  * A mutable reactive value that supports draft editing and publishing. Essentially,
- * this represents an input buffer for a [MutableReactive].
+ * this represents an input buffer for a "published" [MutableReactive].
  *
  * A [Draft] copies its [published] value until you set a new value in the draft. After
  * you set a new value, the draft keeps this change separate from the published value.
@@ -64,6 +64,17 @@ private class BaseDraft<T> private constructor(
     override fun cancel() { buffer.reset() }
 }
 
+/**
+ * Creates a [Draft] using the specified [MutableReactive] as the published value.
+ * */
 fun <T> Draft(published: MutableReactive<T>): Draft<T> = BaseDraft(published)
+
+/**
+ * Creates a [Draft] where the published value is the provided [initialValue]
+ * */
 fun <T> Draft(initialValue: T): Draft<T> = BaseDraft(Signal(initialValue))
+
+/**
+ * Creates a [Draft] where the published value is calculated based off the provided [initialValue] calculation.
+ * */
 fun <T> Draft(initialValue: ReactiveContext.() -> T): Draft<T> = BaseDraft(MutableRemember(useLastWhileLoading = true, initialValue = initialValue))
