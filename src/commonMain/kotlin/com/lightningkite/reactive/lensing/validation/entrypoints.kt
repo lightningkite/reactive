cef8bbbbe55f61d91a390b30d9fb8b857f5e875b
iHoonter <huntdawghero@gmail.com>
2025-08-10 18:21:50 -0600

    Added optional root reporting

--- src/commonMain/kotlin/com/lightningkite/reactive/lensing/validation/entrypoints.kt
+++ src/commonMain/kotlin/com/lightningkite/reactive/lensing/validation/entrypoints.kt
@@ -9,27 +9,31 @@ import kotlin.getValue
 import kotlin.setValue
 
 private open class RootValidated<T>(
-    private val wrapped: Reactive<T>
+    private val wrapped: Reactive<T>,
+    reportTo: IssueNode?
 ) : Validated<T>, Reactive<T> by wrapped {
-    override val node: IssueNode = IssueNode(null)
+    override val node: IssueNode = reportTo?.child() ?: IssueNode(null)
 }
 
 private class RootMutableValidated<T>(
-    private val wrapped: MutableReactive<T>
-) : MutableValidated<T>, RootValidated<T>(wrapped) {
+    private val wrapped: MutableReactive<T>,
+    reportTo: IssueNode?
+) : MutableValidated<T>, RootValidated<T>(wrapped, reportTo) {
     override suspend fun set(value: T) = wrapped.set(value)
 }
 
 private open class RootValidatedValue<T>(
-    private val wrapped: ReactiveValue<T>
+    private val wrapped: ReactiveValue<T>,
+    reportTo: IssueNode?
 ) : ValidatedValue<T>, Listenable by wrapped {
-    override val node: IssueNode = IssueNode(null)
+    override val node: IssueNode = reportTo?.child() ?: IssueNode(null)
     override val value: T get() = wrapped.value
 }
 
 private class RootMutableValidatedValue<T>(
-    private val wrapped: MutableReactiveValue<T>
-) : MutableValidatedValue<T>, RootValidatedValue<T>(wrapped) {
+    private val wrapped: MutableReactiveValue<T>,
+    reportTo: IssueNode?
+) : MutableValidatedValue<T>, RootValidatedValue<T>(wrapped, reportTo) {
     override var value: T by wrapped::value
 }
 
@@ -39,23 +43,25 @@ private class RootMutableValidatedValue<T>(
  * If the receiver is already [Validated], it is returned as-is. Otherwise, this creates the root of the validation tree for the reactive value.
  * This enables validation and issue tracking for the reactive value. See [IssueTracking] for more details about validation trees.
  *
+ * @param reportTo Optionally provide a parent [IssueNode] to aggregate issues. This is only used if the receiver is not already validated and a new root validation node is constructed. Supplying a parent node allows you to aggregate validation issues across multiple unrelated data structures.
  * @return A [Validated] instance wrapping the receiver.
  *
  * @see IssueTracking
  */
-fun <T> Reactive<T>.validated(): Validated<T> = this as? Validated<T> ?: RootValidated(this)
+fun <T> Reactive<T>.validated(reportTo: IssueNode? = null): Validated<T> = this as? Validated<T> ?: RootValidated(this, reportTo)
 
 /**
  * Wraps a [MutableReactive] as a [MutableValidated].
  *
  * If the receiver is already a [MutableValidated], it is returned as-is. Otherwise, this creates the root of the validation tree for the reactive value.
- * This enables validation and issue tracking for the reactive value. See [IssueTracking] for more details about validation trees.
+ * This enables validation and issue tracking for the reactive value and its lenses. See [IssueTracking] for more details about validation trees.
  *
+ * @param reportTo Optionally provide a parent [IssueNode] to aggregate issues. This is only used if the receiver is not already validated and a new root validation node is constructed. Supplying a parent node allows you to aggregate validation issues across multiple unrelated data structures.
  * @return A [MutableValidated] instance wrapping the receiver.
  *
  * @see IssueTracking
  */
-fun <T> MutableReactive<T>.validated(): MutableValidated<T> = this as? MutableValidated<T> ?: RootMutableValidated(this)
+fun <T> MutableReactive<T>.validated(reportTo: IssueNode? = null): MutableValidated<T> = this as? MutableValidated<T> ?: RootMutableValidated(this, reportTo)
 
 /**
  * Wraps a [ReactiveValue] as a [ValidatedValue].
@@ -63,20 +69,22 @@ fun <T> MutableReactive<T>.validated(): MutableValidated<T> = this as? MutableVa
  * If the receiver is already [ValidatedValue], it is returned as-is. Otherwise, this creates the root of the validation tree for the reactive value.
  * This enables validation and issue tracking for the reactive value. See [IssueTracking] for more details about validation trees.
  *
+ * @param reportTo Optionally provide a parent [IssueNode] to aggregate issues. This is only used if the receiver is not already validated and a new root validation node is constructed. Supplying a parent node allows you to aggregate validation issues across multiple unrelated data structures.
  * @return A [ValidatedValue] instance wrapping the receiver.
  *
  * @see IssueTracking
  */
-fun <T> ReactiveValue<T>.validated(): ValidatedValue<T> = this as? ValidatedValue<T> ?: RootValidatedValue(this)
+fun <T> ReactiveValue<T>.validated(reportTo: IssueNode? = null): ValidatedValue<T> = this as? ValidatedValue<T> ?: RootValidatedValue(this, reportTo)
 
 /**
  * Wraps a [MutableReactiveValue] as a [MutableValidatedValue].
  *
  * If the receiver is already a [MutableValidatedValue], it is returned as-is. Otherwise, this creates the root of the validation tree for the reactive value.
- * This enables validation and issue tracking for the reactive value. See [IssueTracking] for more details about validation trees.
+ * This enables validation and issue tracking for the reactive value and its lenses. See [IssueTracking] for more details about validation trees.
  *
+ * @param reportTo Optionally provide a parent [IssueNode] to aggregate issues. This is only used if the receiver is not already validated and a new root validation node is constructed. Supplying a parent node allows you to aggregate validation issues across multiple unrelated data structures.
  * @return A [MutableValidatedValue] instance wrapping the receiver.
  *
  * @see IssueTracking
  */
-fun <T> MutableReactiveValue<T>.validated(): MutableValidatedValue<T> = this as? MutableValidatedValue<T> ?: RootMutableValidatedValue(this)
+fun <T> MutableReactiveValue<T>.validated(reportTo: IssueNode? = null): MutableValidatedValue<T> = this as? MutableValidatedValue<T> ?: RootMutableValidatedValue(this, reportTo)
